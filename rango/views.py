e67b1f356efbda591cb330a9af37578568dbdf3c
Kajsa Berglof Morales <3016859b@student.gla.ac.uk>
2026-01-27 16:30:35 +0000

    chapter 4 ex- adding images to the index and about page

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    #construct a disctionary to pass to the template engine as its context
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #render the response and send it back to the client
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-
+    context_dict = {'boldmessage': "This tutorial has been put together by Kajsa."}
+    return render(request, 'rango/about.html', context=context_dict)
