c10b2c727d51705d31c833a61e95297ac6a4516f
Kajsa Berglof Morales <3016859b@student.gla.ac.uk>
2026-02-05 13:39:49 +0000

    chapter 9-update all html, added register

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
@@ -129,4 +128,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
